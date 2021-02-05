62aeebdc9d2f08ee808aee39120d29b7db481cca
Henrique Lima <henriquemlima7@gmail.com>
2021-02-05 10:21:33 +0000

    Implement basic cfn parser

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs'
+import { CloudFormationParser } from './platform-mapping/cloudformation/parser'
+import * as YAML from 'yaml'
+
+
+// const yamlSample = fs.readFileSync('./sample-cfn-template.yaml').toString()
+// const cfnTemplate = YAML.parse(yamlSample)
+
+const cloudformationDir = `./platform-mapping/cloudformation`
+const cloudformationOutputDir = `${cloudformationDir}/sample-outputs`
+if(!fs.existsSync(cloudformationOutputDir)) fs.mkdirSync(cloudformationOutputDir)
+
+fs.readdirSync(`${cloudformationDir}/sample-inputs`).forEach(sampleInputFilename => {
+    const cfnTemplate = JSON.parse(fs.readFileSync(`${cloudformationDir}/sample-inputs/${sampleInputFilename}`, 'utf8'))
+    const parser = new CloudFormationParser(cfnTemplate)
+    parser.parse()
+})
