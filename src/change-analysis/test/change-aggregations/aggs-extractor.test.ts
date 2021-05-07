61661886d6a5d9a07ae8b9bdf07d73434779ff9d
Henrique Lima <henlima@amazon.com>
2021-05-07 08:48:25 +0100

    Add aggregation tests content

--- src/change-analysis/test/change-aggregations/aggs-extractor.test.ts
+++ src/change-analysis/test/change-aggregations/aggs-extractor.test.ts
@@ -1,4 +1,5 @@
 import {
+    ComponentOperation,
     Transition,
 } from "change-cd-iac-models/model-diffing";
 import {
@@ -10,6 +11,7 @@ import {
     DiffCreator,
 } from "../../model-diffing";
 import { diffTestCase1 } from "../default-test-cases/infra-model-diff";
+import { Aggregation } from "change-cd-iac-models/aggregations";
 
 const dir = `test/change-aggregations`;
 
@@ -20,7 +22,25 @@ const {
 test('Extract Agg of operation type, component type and component subtype', () => {
     
     const diff = diffTestCase1();
-    const groups = extractComponentOperationsAggs(diff, new Map());
+    const aggs = extractComponentOperationsAggs(diff, new Map());
+
+    expect(aggs.length).toBe(1);
+    expect(aggs[0].characteristics).toMatchObject({"RISK":"unknown"});
+    const subAggs1 = aggs[0].subAggs;
+    expect(subAggs1.length).toBe(2);
+    expect(subAggs1[0].characteristics).toMatchObject({
+        "Component Subtype": "AWS::IAM::Role",
+        "Component Type": "resource",
+        "Operation Type": "UPDATE"
+    });
+    expect(subAggs1[0].subAggs.length).toBe(1);
+    expect(subAggs1[1].characteristics).toMatchObject({
+        "Component Subtype": "AWS::EC2::Instance",
+        "Component Type": "resource",
+        "Operation Type": "UPDATE"
+    });
+    expect(subAggs1[1].subAggs.length).toBe(1);
+    
 });
 
 
@@ -29,5 +49,9 @@ test('Group operations from big template', () => {
     const newModel = new CDKParser(readSampleInput('kessel-run-stack-after.json')).parse();
     
     const diff = new DiffCreator(new Transition({v1: oldModel, v2: newModel})).create();
-    const aggregations = extractComponentOperationsAggs(diff, new Map());
+    const aggs = extractComponentOperationsAggs(diff, new Map());
+
+    expect(aggs.length).toBe(1);
+    expect(aggs[0].subAggs.length).toBe(10)
+    expect(aggs[0].entities.size).toBe(86)
 });
