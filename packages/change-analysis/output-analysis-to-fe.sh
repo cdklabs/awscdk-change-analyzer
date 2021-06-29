41a984bc22b43a985308826e403c67da67137021
Bryan Pan <bryanpan342@gmail.com>
2021-06-29 10:07:09 -0700

    switch fifinet to reference npm package instead of github

--- packages/change-analysis/output-analysis-to-fe.sh
+++ packages/change-analysis/output-analysis-to-fe.sh
@@ -1,6 +1,6 @@
 # Generates and exports change report to fe/model-diff-example.json
 # Considering there is a ""../experiment templates" folder with different subfolders containing before.json, after.json and rules.json
-npm start -- \
+node out/index.js \
  "../../experiment templates/$1/before.json" \
  "../../experiment templates/$1/after.json" \
  "../../experiment templates/$1/rules.json" \
