caa6146d9a362a4fde1f8c79c5733e9d36900e16
Bryan Pan <bryanpan342@gmail.com>
2021-08-09 17:38:22 -0700

    dynamically load the data.json file for local development

--- packages/web-app/src/index.tsx
+++ packages/web-app/src/index.tsx
@@ -1,12 +1,14 @@
+import 'intro.js/introjs.css';
+import { ChangeAnalysisReport, JSONDeserializer } from 'cdk-change-analyzer-models';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Intro } from './Intro';
-import 'intro.js/introjs.css';
 import { REPORT_PLACEHOLDER } from './config';
-import { JSONDeserializer } from 'cdk-change-analyzer-models/export/json-deserializer';
-import { ChangeAnalysisReport } from 'cdk-change-analyzer-models/change-analysis-report';
-import data from '../data.json';
 
-const changeReport = new JSONDeserializer<ChangeAnalysisReport>().deserialize(IS_PRODUCTION ? REPORT_PLACEHOLDER : JSON.stringify(data));
+// To enable for local development, this index file is generated on build so we are
+// dynamically requiring the data file for `yarn dev`, but rely on string replacement
+// for production environments after the template is created
+const report = IS_PRODUCTION ? REPORT_PLACEHOLDER : JSON.stringify(require('../data.json'));
+const changeReport = new JSONDeserializer<ChangeAnalysisReport>().deserialize(report);
 ReactDOM.render(<><Intro /><App changeReport={changeReport} /></>, document.getElementById('root'));
