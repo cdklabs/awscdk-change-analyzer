984cf7231a5a879a34e596aa6bab37b239458fa2
Bryan Pan <bryanpan342@gmail.com>
2021-09-08 10:09:39 -0700

    feat: integrate @aws-c2a/visualizer into cli (#51)
    Generate an html file that visualizes the diff tree of your app.
    
    > Currently, only evaluates the first object in the template tree and **does not** 
    support nested stacks
    
    ```sh
    aws-c2a vis --app='path/to/assembly/' --out='path/to/index.html'
    ```

--- packages/aws-c2a/lib/index.ts
+++ packages/aws-c2a/lib/index.ts
@@ -1,4 +1,5 @@
-export * from './toolkit';
 export * from './c2a-host';
 export * from './cfn-traverser';
 export * from './cloud-assembly';
+export * from './toolkit';
+export * from './toolkit-utils';
